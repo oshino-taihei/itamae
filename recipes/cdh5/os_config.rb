a4a690d9a39c1b37865e996a6a39a46a212d5d15
oshino-taihei <oshino.taihei+github@gmail.com>
2016-10-05 02:40:49 -0400

    distribute authorized_keys

--- recipes/cdh5/os_config.rb
+++ recipes/cdh5/os_config.rb
@@ -5,3 +5,11 @@ remote_file "/etc/hosts" do
   source "remote_files/hosts"
 end
 
+# /root/.ssh/authorized_keys配置
+remote_file "/root/.ssh/authorized_key" do
+  owner "root"
+  group "root"
+  mode "600"
+  source "remote_files/authorized_keys"
+end
+

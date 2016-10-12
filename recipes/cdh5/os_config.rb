b87e2318b22c9751b2ac61cc2810b13ae3002dd6
oshino-taihei <oshino.taihei+github@gmail.com>
2016-10-12 06:09:58 -0400

    ホスト名変更レシピを追加

--- recipes/cdh5/os_config.rb
+++ recipes/cdh5/os_config.rb
@@ -1,10 +1,32 @@
-# /etc/hosts配置
+### ホスト名変更 ###
+# ホスト名変更時は手動でOS再起動
 remote_file "/etc/hosts" do
   owner "root"
   group "root"
-  source "remote_files/hosts"
+  source "remote_files/etc/hosts"
 end
 
+template "/etc/hostname" do
+  owner "root"
+  group "root"
+  source "templates/etc/hostname.erb"
+  variables(hostname: node[:os][:hostname])
+end
+
+template "/etc/sysconfig/network" do
+  owner "root"
+  group "root"
+  source "templates/etc/sysconfig/network.erb"
+  variables(hostname: node[:os][:hostname])
+end
+
+remote_file "/etc/cloud/cloud.cfg" do
+  owner "root"
+  group "root"
+  source "/etc/cloud/cloud.cfg"
+end
+
+### パスワードなしssh ###
 # /root/.ssh/authorized_keys配置
 remote_file "/root/.ssh/authorized_key" do
   owner "root"
